c66163aa4023b48e7c872d252f06102edcf82973
doyuni <gdypc@naver.com>
2019-11-15 10:07:42 +0900

    [Build Test] build script 작성

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo Build Image
+docker image build -t linking_server:latest .
+echo Create container
+docker rm -f LINKING_SERVER
+docker create --name LINKING_SERVER -p 1024:1024 linking_server:latest
+
+echo Start Server
+docker start LINKING_SERVER
+
+echo Complete Build 
