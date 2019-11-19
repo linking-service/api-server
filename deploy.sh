00dbeb4b6bfd114089ef1f656f1f2ade04976156
doyuni <gdypc@naver.com>
2019-11-19 19:58:29 +0900

    [path 변경]deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Build Image
-docker image build -t linking_server:latest .
+docker image build -t linking_server:latest /home/linking/api-server/
 echo Create container
 docker rm -f LINKING_SERVER
 docker create --name LINKING_SERVER -p 1024:1024 linking_server:latest
