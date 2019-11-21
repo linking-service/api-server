7c3fcb3b7db9d7367744f2703ddf474067ccfe22
vincentj2 <vincentj@naver.com>
2019-11-22 00:37:44 +0900

    [API 추가]디렉토리 내부 모든 링크 데이터 전달

--- routes/link.js
+++ routes/link.js
@@ -62,7 +62,7 @@ router.post("/:dir_id/saved", async (req, res) => {
     res.status(202).json(metadata);
 });
 
-//링크 데이터 전달
+//링크 데이터 전달 디렉토리 id/링크 id
 router.post("/:dir_id/:link_id" , async (req, res) => {
     let result = null;
 
@@ -70,6 +70,31 @@ router.post("/:dir_id/:link_id" , async (req, res) => {
         result = await linkModel.find({
             dir_id: req.params.dir_id,
             link_id: req.params.link_id
+        },{_id:0, link :1,tag:1,desc:1,meta_desc:1,
+            meta_imgUrl: 1,meta_title: 1,read_status: 1,created_time: 1, revised_time: 1
+        });
+        console.log("DB find");
+        return await res.json(result);
+    } catch (err) {
+        return res.status(500).json({
+            msg: "DB Find error"
+        });
+    }
+    if (!result) {
+        res.status(404).json({
+            msg: "Cannot find the directory id"
+        });
+        return;
+    }
+});
+
+//디렉토리 내부 모든 링크 호출
+router.post("/:dir_id" , async (req, res) => {
+    let result = null;
+
+    try {
+        result = await linkModel.find({
+            dir_id: req.params.dir_id,
         },{_id:0, link :1,tag:1,desc:1,meta_desc:1,
             meta_imgUrl: 1,meta_title: 1,read_status: 1,created_time: 1, revised_time: 1
         });
