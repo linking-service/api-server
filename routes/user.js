6f69c9274a6013b434bdd7af4856a19af8d96b60
vincentj2 <vincentj@naver.com>
2019-12-01 06:11:27 +0900

    [API 추가]user delete

--- routes/user.js
+++ routes/user.js
@@ -25,4 +25,16 @@ router.post('/', function(req,res){
     })
 });
 
+//사용자 탈퇴
+router.get("/:display_name/delete",function (req,res){
+    userModel.deleteOne({display_name: req.params.display_name},function (err) {
+        if(err) {
+            console.log(err);
+            res.status(404).send('delete fail');
+        }else{
+            res.status(200).send('delete user');
+        }
+    });
+})
+
 module.exports = router;
