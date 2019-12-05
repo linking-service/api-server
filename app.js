46c93e62ad73010d99977be1629658f29af62288
doyuni <gdypc@naver.com>
2019-12-05 14:57:00 +0900

    [nginx] 무중단 test

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) =>{
 });
 
 app.get('/', (req,res)=>{
-    res.send("Linking Service");
+    res.send("Welcome Linking Service");
 });
 
 app.set('views', path.join(__dirname, 'views'));
