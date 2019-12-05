0ec508584b50badf492f4fa3c249a01b18667388
vincentj2 <vincentj@naver.com>
2019-12-06 04:06:05 +0900

    [API 추가]ADD message number & call mailList

--- routes/mail.js
+++ routes/mail.js
@@ -28,8 +28,9 @@ router.post('/:display_name/:sender/:type', async (req,res)=>{
               message: senderName + "님이 " +dirName+ " 디렉토리를 공유했습니다."
           })
           Mail.save();
+          return res.status(200);
 
-          return res.json(Mail.message);
+          //return res.json(Mail.message);
       }
 
       else if(type ==1){
@@ -39,8 +40,9 @@ router.post('/:display_name/:sender/:type', async (req,res)=>{
               message: senderName + "님이 디렉토리를 공유를 거절했습니다."
           })
           Mail.save();
+          return res.status(200);
 
-          return res.json(Mail.message);
+         // return res.json(Mail.message);
       }
       else if(type ==2){
           const Mail = new mailModel({
@@ -49,14 +51,28 @@ router.post('/:display_name/:sender/:type', async (req,res)=>{
               message: senderName + "님이 디렉토리를 공유를 수락했습니다."
           })
           Mail.save();
+          return res.status(200);
 
-          return res.json(Mail.message);
+         // return res.json(Mail.message);
       }
   });
-
-
-
 });
 
+//메세지 갯수 알림
+router.get("/:display_name/mailnumber", async(req,res)=>{
+    mailModel.find({display_name:req.params.display_name},{_id:0, mail_id :1},function (err,mail_id){
+        if(err) console.log(err);
 
+        return res.json({mailnumber :mail_id.length});
+    })
+})
+
+//유저 메세지함 출력
+router.get("/:display_name/mailList", async(req,res)=>{
+    mailModel.find({display_name:req.params.display_name},{_id:0, message:1},function (err, message) {
+        if(err) console.log(err);
+
+        return res.json(message);
+    })
+} )
 module.exports = router;
