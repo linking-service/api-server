77840e9e4d7f198745afcd8bd53b4c8b727f3853
vincentj2 <vincentj@naver.com>
2019-12-09 19:37:42 +0900

    fix

--- routes/user.js
+++ routes/user.js
@@ -16,24 +16,26 @@ router.post('/', async (req,res)=>{
                     display_name : req.body.name,
                 });
 
+            // user.save(function (err) {
+            //     if(err) return console.log(err);
+            //     console.log('user information saved!');
+            //   });
             user.save(function (err) {
                 if(err) return console.log(err);
                 console.log('user information saved!');
-              });
-            // let displayName;
-            // displayName= userModel.find({email:req.body.email},{_id:0, display_name:1}).lean();
-            // let code = {"code" :0};
-            // Object.assign(code,displayName[0]);
-            //
-            // return res.json(code);
-         //   return res.status(404).json({code : 0});
-        }
-        let displayName;
-        displayName= await userModel.find({email:req.body.email},{_id:0, display_name:1}).lean();
-        let code = {"code" :0};
-        Object.assign(code,displayName[0]);
+            });
+            let code = {"code" :0};
+            Object.assign(code,req.body.display_name);
 
-        return res.json(code);
+            return res.json(code);
+         //  return res.status(404).json({code : 0});
+        }
+        // let displayName;
+        // displayName= await userModel.find({email:req.body.email},{_id:0, display_name:1}).lean();
+        // let code = {"code" :0};
+        // Object.assign(code,displayName[0]);
+        //
+        // return res.json(code);
        // res.json({code : 1});
     })
 
