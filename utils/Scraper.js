f308e0e6d15416e602d71aa803cd96608996db86
vincentj2 <vincentj@naver.com>
2019-12-11 19:29:16 +0900

    fix scraper

--- utils/Scraper.js
+++ utils/Scraper.js
@@ -7,22 +7,22 @@ class Scraper {
     }
 
     async getResponse() {
-        const {
-            data
-        } = await axios.get(this.url);
+        const { data } = await axios.get(this.url);
+
         return data;
     }
 
-    filterByTarget({
-                       data,
-                       target
-                   }) {
-        return data.filter(
-            elem =>
-                elem.itemprop === target ||
-                elem.property === target ||
-                elem.name === target
-        );
+    filterByTarget({ data, target }) {
+        return data.filter(elem => {
+            const itemprop = elem.itemprop ? elem.itemprop : "";
+            const property = elem.property ? elem.property : "";
+            const name = elem.itemprop ? elem.name : "";
+            return (
+                itemprop.includes(target) ||
+                property.includes(target) ||
+                name.includes(target)
+            );
+        });
     }
 
     getData(html) {
@@ -34,49 +34,36 @@ class Scraper {
             if (attrib.itemprop || attrib.property || attrib.name)
                 data = [...data, attrib];
         });
+
+        const pageTitle = $("title").text();
+
         const [title, ...titleArg] = this.filterByTarget({
             data,
             target: "title"
-        });
-        const [ogTitle, ...ogTitleArg] = this.filterByTarget({
-            data,
-            target: "og:title"
         });
         const [desc, ...descArg] = this.filterByTarget({
             data,
             target: "description"
         });
-        const [ogDesc, ...ogDescArg] = this.filterByTarget({
-            data,
-            target: "og:description"
-        });
         const [imgUrl, ...imgUrlArg] = this.filterByTarget({
             data,
             target: "image"
         });
-        const [ogImgUrl, ...ogImgUrlArg] = this.filterByTarget({
-            data,
-            target: "og:image"
-        });
 
         return {
-            title: ogTitle ? ogTitle.content : title.content,
-            desc: ogDesc ? ogDesc.content : desc.content,
-            imgUrl: ogImgUrl ? ogImgUrl.content : imgUrl.content
+            title: title ? title.content : pageTitle ? pageTitle : this.url,
+            desc: desc ? desc.content : this.url,
+            imgUrl: imgUrl ? imgUrl.content : ""
         };
     }
 }
 
-// Usage, testcode
-
 // (async () => {
-//   const scraper = new Scraper("https://www.naver.com/");
+//   const scraper = new Scraper("https://react.semantic-ui.com/elements/icon/");
 //   const resData = await scraper.getResponse();
 //   const metaData = scraper.getData(resData);
 
 //   console.log(metaData);
 // })();
-
-// 끝
 
 module.exports = Scraper;
